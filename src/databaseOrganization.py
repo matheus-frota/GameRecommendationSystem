3e911a14b2bd44c19400da6325a8344407a5ebee
Matheus <matheusfrota@hotmail.com.br>
2019-11-15 13:49:35 -0300

    ~Concluindo fase beta de implementação do recomendador de jogos

--- src/databaseOrganization.py
+++ src/databaseOrganization.py
@@ -11,16 +11,16 @@ def openCSV():
     return df.query("purchase == 'play'").drop(["purchase","delete"],axis = 1)
 
 def treatDataset():
-    df = openCSV()
-    nameGameUnique = df["gameName"].unique()
-    nameGameID = {Game:GameID for GameID,Game in zip(range(1,len(nameGameUnique)),nameGameUnique)}
-    df["gameNameID"] = df["gameName"].replace(nameGameID)
+    df = filterDataset()
+    nameGameUnique = df.index.unique()
+    nameGameID = {Game:GameID for GameID,Game in zip(range(1,len(nameGameUnique)+1),nameGameUnique)}
+    df = df.rename(index = nameGameID)
     return df, nameGameID
 
 def filterDataset(mostPlayed = 0.2):
-    df, nameGameID = treatDataset()
-    numberUniqueGames = len(df["gameNameID"].unique())
+    df = openCSV()
+    numberUniqueGames = len(df["gameName"].unique())
     selectedQuantity = int(numberUniqueGames*mostPlayed)
-    gameSelect = df.groupby(["gameNameID"])["userID"].count().sort_values(ascending = False)[:selectedQuantity].keys()
-    sparseMatrix = df.pivot_table(index='gameNameID',columns='userID',values='hours').fillna(0)
-    return pd.DataFrame(sparseMatrix.iloc[gameSelect]), nameGameID
+    gameSelect = df.groupby(["gameName"])["userID"].count().sort_values(ascending = False)[:selectedQuantity].keys()
+    sparseMatrix = df.pivot_table(index='gameName',columns='userID',values='hours').fillna(0)
+    return sparseMatrix.loc[gameSelect]
--- src/knn.py
+++ src/knn.py
@@ -1,11 +1,30 @@
 import pandas as pd
 import databaseOrganization as do
 from difflib import SequenceMatcher
+import numpy as np
 
 
-def train(data, gameNameUser):
+def euclidianDistance(x,y):
+    return np.sqrt(np.sum((y - x)**2))
 
-    print(data)
+def cosineSimilarity(x,y):
+    return np.sqrt(np.sum(x*y))/(np.sqrt(np.sum(x**2))*np.sqrt(np.sum(y**2)))
+
+# Definindo função ordenarDicionario().
+def ordenarValoresDicionario(dicionario):
+    # Variavel auxiliar
+    armazenarResultadoOrdenado = []
+    for i in sorted(dicionario, key = dicionario.get, reverse = False):
+        armazenarResultadoOrdenado.append((i,dicionario[i]))
+    return armazenarResultadoOrdenado
+
+def train(data, gameNameData):
+    distanceGames = {}
+    gameNameX = data[data.index == gameNameData].values
+    gameNameX = np.asarray(gameNameX).ravel()
+    for index,gameNameY in data.iterrows():
+        distanceGames[index] = cosineSimilarity(gameNameX,gameNameY.values)
+    return ordenarValoresDicionario(distanceGames)    
     
 
 def similarNames(gameNameUser, allGamesName):
@@ -20,9 +39,13 @@ def similarNames(gameNameUser, allGamesName):
         return allGamesName[gamesSimilarNames[0]]
 
 def recommender(k, gameNameUser):
-    data, nameGameID = do.filterDataset()
+    data, nameGameID = do.treatDataset()
     gamesNameData = similarNames(gameNameUser, nameGameID)
     if gamesNameData == None:
         print("Nenhum jogo com esse nome foi encontrado!")
     else:
-        print(gamesNameData)
+        gamesRecommender = train(data,gamesNameData)
+        replaceID = {ID:name for name,ID in zip(nameGameID.keys(),nameGameID.values())}
+        for game in gamesRecommender[1:k+1]:
+            print(replaceID[game[0]])
+        
