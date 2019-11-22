3942262ccb905c00ff2cc73730fb66fe0d9bcbf6
Matheus <matheusfrota@hotmail.com.br>
2019-11-21 21:41:13 -0300

    Adicionando arquivos e Api web Flask

---
+++ app/api.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template,jsonify
+#from src.knn import recommender
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/join', methods=['GET','POST'])
+def my_form_post():
+    text1 = request.form['text1']
+    text2 = request.form['text2']
+    #combine = recommender(text1,text2)
+    combine = "aaaaaaaaaaaaaaa"
+    result = {
+        "output": combine
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    author='Matheus Frota',
+    author_email='matheusfrota@hotmail.com.br',
+    packages=['src','app']
+)
